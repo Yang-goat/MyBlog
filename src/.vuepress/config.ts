faff6455dc7763408fbdf2697d9a750bc463770f
Yang-goat <1700425119@qq.com>
2025-06-19 19:06:09 +0800

    修改base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/MyBlog/",
+  base: "/",
 
   lang: "zh-CN",
   title: "Goat_Yang",
