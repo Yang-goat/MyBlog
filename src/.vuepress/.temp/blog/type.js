26f6d1c86c309ef7abd73199357997618b78a536
Yang-goat <1700425119@qq.com>
2025-08-30 21:15:19 +0800

    数模国赛latex模板更新

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[2,3,0,1,4,5,6,7,8,9,10,11,12,13,22,17,14,15,16,18,19,20,21]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[2,3,0,1,4,5,6,7,8,9,10,11,12,13,22,17,14,15,16,18,19,20,21]}}}");
+export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[0,1,2,3,13,14,15,16,17,18,19,20,21,22,9,4,5,6,7,8,10,11,12]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[0,1,2,3,13,14,15,16,17,18,19,20,21,22,9,4,5,6,7,8,10,11,12]}}}");
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
