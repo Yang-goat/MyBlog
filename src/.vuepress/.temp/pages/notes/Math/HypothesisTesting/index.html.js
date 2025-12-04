d840d4f7e470b797fa498e3cdfcd95e93be2f137
Yang-goat <1700425119@qq.com>
2025-12-04 13:04:39 +0800

    文章定期增量更新

---
+++ src/.vuepress/.temp/pages/notes/Math/HypothesisTesting/index.html.js
@@ -0,0 +1,16 @@
+import comp from "/media/goat/双系统共享/project/Blog/myBlog/src/.vuepress/.temp/pages/notes/Math/HypothesisTesting/index.html.vue"
+const data = JSON.parse("{\"path\":\"/notes/Math/HypothesisTesting/\",\"title\":\"假设检验\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"假设检验\",\"index\":false,\"article\":false,\"icon\":\"h\",\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"假设检验\\\"}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://github.com/Yang-goat/MyBlog/notes/Math/HypothesisTesting/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"Goat_Yang\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"假设检验\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"readingTime\":{\"minutes\":0.04,\"words\":11},\"filePathRelative\":\"notes/Math/HypothesisTesting/README.md\",\"excerpt\":\"\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ src/notes/Math/HypothesisTesting/README.md
@@ -0,0 +1,6 @@
+---
+title: 假设检验
+index: false
+article: false
+icon: h
+---
