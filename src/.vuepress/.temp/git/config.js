b0263d281034c461e02e570b96327b7a9b9630c0
Yang-goat <1700425119@qq.com>
2026-03-18 20:44:08 +0800

    优化宝藏站点样式

--- src/.vuepress/.temp/git/config.js
+++
@@ -1,7 +0,0 @@
-import { GitContributors } from "/media/goat/data/project/Blog/myBlog/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-rc.108_vuepress@2.0.0-rc.23_@vuepress+bundler-vite@2.0.0-rc._29aa02cc064c6fa7151f11b85de4b320/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-  },
-};
--- src/.vuepress/.temp/markdown-stylize/config.js
+++
@@ -1,3 +0,0 @@
-import "/media/goat/data/project/Blog/myBlog/node_modules/.pnpm/@mdit+plugin-spoiler@0.21.0_markdown-it@14.1.0/node_modules/@mdit/plugin-spoiler/spoiler.css"
-
-
